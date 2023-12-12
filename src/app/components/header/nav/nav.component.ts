7799b5bad2316c6266631f4a4ce7b5f6b8d9f8b8
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2023-12-12 22:34:00 +0600

    Desktop v of header was finished

--- src/app/components/header/nav/nav.component.ts
+++ src/app/components/header/nav/nav.component.ts
@@ -5,8 +5,10 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
-
+  phoneNumber = '+7(771) 224 31-18';
+  email = 'info@oneway.kz';
+  contactAvailability = 'с 10:00 до 21:00';
 }
