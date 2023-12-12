2424c617dee58af0d50746e40a481c31ab3717dd
Sayat Kalnazar <kalnazar.sayat@gmail.com>
2023-12-12 23:24:55 +0600

    Entry for projects was done

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,12 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  rectangles = new Array(3).fill({ isVisible: false });
 
+  ngOnInit(): void {
+    this.checkVisibility();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  onWindowScroll() {
+    this.checkVisibility();
+  }
+
+  private checkVisibility() {
+    const elements = document.querySelectorAll('.entry__rectangles .rectangle');
+    let delay = 0;
+    const delayIncrement = 1000;
+    elements.forEach((el, index) => {
+      const rect = el.getBoundingClientRect();
+      if (rect.top < window.innerHeight) {
+        this.rectangles[index].isVisible = true;
+      }
+    });
+  }
 }
